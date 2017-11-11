6d67ed1426d0d5e342c3d470d8ffaff870ba5720
Joacim Löwgren <joacim.lowgren@gmail.com>
2017-11-11 12:49:26 +0100

    Handle multiple HTML assets

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const ESI = require('nodesi');
+const path = require('path');
 const { RawSource } = require('webpack-sources');
 
-const FILENAME = 'index.html';
+class EsiWebpackPlugin {
+  constructor(options = {}) {
+    const {
+      baseUrl,
+      onError,
+      processOptions = {},
+    } = options;
 
-function EsiWebpackPlugin(options = {}) {
-  const {
-    baseUrl,
-    onError,
-    processOptions = {},
-  } = options;
+    this.options = options;
 
-  this.options = options;
+    this.esi = new ESI({
+      baseUrl,
+      onError,
+      processOptions,
+    });
+  }
 
-  this.esi = new ESI({
-    baseUrl,
-    onError,
-    processOptions,
-  });
-}
+  apply(compiler) {
+    const { processOptions } = this.options;
 
-EsiWebpackPlugin.prototype.apply = function apply(compiler) {
-  const { processOptions } = this.options;
-
-  compiler.plugin('emit', (compilation, callback) => {
-    this.esi.process(compilation.assets[FILENAME].source(), processOptions)
-      .then((result) => {
-        // eslint-disable-next-line
-        compilation.assets[FILENAME] = new RawSource(result);
-        callback();
+    compiler.plugin('emit', ({ assets }, callback) => {
+      Object.keys(assets).forEach((filename) => {
+        if (path.extname(filename) === '.html') {
+          this.esi.process(assets[filename].source(), processOptions)
+            .then((result) => {
+              // eslint-disable-next-line
+              assets[filename] = new RawSource(result);
+              callback();
+            })
+            .catch(err => callback(err));
+        }
       });
-  });
-};
+    });
+  }
+}
 
 module.exports = EsiWebpackPlugin;
